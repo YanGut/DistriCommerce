0a23d0fba00716fb18f1e059abb857d6e11ccea7
Yan Gutberg <guttlauncher@gmail.com>
2025-04-12 18:12:56 -0300

    :tada: start project

---
+++ src/test/java/com/unifor/DistriCommerce/DistriCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unifor.DistriCommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistriCommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
